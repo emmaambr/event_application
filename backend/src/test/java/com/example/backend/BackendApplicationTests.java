f3087c09f831914045c5fd1d232b3c8d35e9f99b
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-01-29 19:03:07 +0100

    resolve issues

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.event;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
