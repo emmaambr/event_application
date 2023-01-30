cd25eb235cc3d3b370c917254128d8ff46fd63e6
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-01-30 23:49:20 +0100

    Event entity completed

--- backend/src/main/java/com/example/backend/entity/Event.java
+++ backend/src/main/java/com/example/backend/entity/Event.java
@@ -1,15 +1,37 @@
 package com.example.backend.entity;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import jakarta.persistence.*;
 
+@Entity
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id; 
+    private Long id; 
+    private String title;
+    private String description; 
+    private LocalDate date; 
+    private LocalTime time; 
+    private int ageLimit;
+    private Double cost; 
 
-    public Event(Long id) {
+    // private String oranizer; (+ get & set)
+
+    public Event() {
+    
+    }
+
+    public Event(Long id, String title, String description, LocalDate date, LocalTime time, int ageLimit, Double cost) {
         this.id = id; 
+        this.title = title; 
+        this.description = description; 
+        this.date = date;
+        this.time = time; 
+        this.ageLimit = ageLimit;
+        this.cost = cost; 
     }
 
     public Long getId() {
@@ -18,6 +40,54 @@ public class Event {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
     }
 
 }
