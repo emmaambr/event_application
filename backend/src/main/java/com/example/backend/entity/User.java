93dab2495ad5995b02b6a8e1c0725987528da911
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-02-03 03:43:12 +0100

    User entity

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,5 +1,8 @@
 package com.example.backend.entity;
 
+import javax.validation.constraints.NotBlank;
+
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 
 @Entity
@@ -9,9 +12,30 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public User(Long id) {
-        this.id = id;
+    @NotBlank(message = "username cannot be blank")
+    @Nonnull
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "password cannot be blank")
+    @Nonnull
+    @Column(nullable = false)
+    private String password;
+
+    private String name; 
+    private String email; 
+
+    public User() {
+    
     } 
+
+    public User(Long id, String username, String password, String name, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name; 
+        this.email = email;
+    }
 
     public Long getId() {
         return id;
@@ -21,5 +45,37 @@ public class User {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
