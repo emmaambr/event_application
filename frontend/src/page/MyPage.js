f8155fbaa5eb974fff7a0644bc1250b24f6103de
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-03-06 21:12:10 +0100

    MyPage

---
+++ frontend/src/page/MyPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavComponent from "../component/NavComponent";
+
+const MyPage = () => {
+
+    return (
+        <div> 
+            <NavComponent />
+            mypage
+        </div>
+    )
+}
+
+export default MyPage;
