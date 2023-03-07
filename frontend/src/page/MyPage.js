f82e88d4a5ea24713a42beb0da95fc93457bae32
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-03-07 20:40:50 +0100

    -

--- frontend/src/page/MyPage.js
+++ frontend/src/page/MyPage.js
@@ -1,12 +1,10 @@
 import React from "react";
 import MyEventComponent from "../component/MyEventComponent";
-import NavComponent from "../component/NavComponent";
 
 const MyPage = () => {
 
     return (
         <div> 
-            <NavComponent />
             <MyEventComponent />
         </div>
     )
